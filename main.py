83307dac014c7bee0dfcc8ff7953c96f26198d0f
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-03-03 11:58:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ eng_words = ['Hi','Bye','Task', 'Programm']
 sp_words = ['Hola','Adiós','Tarea', 'Programa']
 score = 0
 
-mode = input("Pick a mode: 0 - add new words, 1 - training: \n")
+mode = input("Elige un modo: 0 - añadir nuevas palabras, 1 - entrenamiento: \n")
 while ((mode != '0') and (mode != '1')):
-    mode = input("Invalid dymbol! Pick either 0 or 1. (0 adds new words, while 1 enables training) \n")
+    mode = input("Símbolo no válido. Elija 0 o 1. (0 añade nuevas palabras, mientras que 1 permite el entrenamiento) \n")
 
 if mode == "1":
-    print("Translate as many words as you can! You have 10 attempts!")
+    print("¡Traduce tantas palabras como puedas! ¡Tienes 10 intentos!")
     for i in range(10):
         number = random.randint(0, len(eng_words))
-        print("How should we translate: " + eng_words[number])
+        print("Cómo deberíamos traducirlo " + eng_words[number])
         if input() == fr_words[number]:
-            print("Great!!!")
+            print("¡¡¡Genial!!!")
             score += 1
         else:
-            print("Nope, not quite... The correct word is - " + eng_words[number])
+            print("No, no del todo... La palabra correcta es - " + eng_words[number])
 else:
-    word = input("Type in an English word: ")
-    translate = input("Type in this word's translation: ")
+    word = input("Escribe una palabra en español: ")
+    translate = input("Escriba la traducción de esta palabra: ")
     if len(word) > 0 and len(translate) > 0:
-        eng_words.append(word)
-        fr_words.append(translate)
-        print("The word was added successfully!")
+        sp_words.append(word)
+        eng_words.append(translate)
+        print("La palabra se ha añadido correctamente")
